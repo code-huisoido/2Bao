07a4e79f5871f3f6571e0092bc19b89db1bfcb3f
huisoido <584331125@qq.com>
2017-07-07 14:24:21 +0800

    feat: 新增天气接口

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,12 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var app = getApp();
+var util = require('../../utils/util.js');
 Page({
   data: {
-    motto: '你就是个二宝',
+    city: '珠海',
+    forecastWeather: '',
+    todayWeather: '',
     userInfo: {}
   },
   //事件处理函数
@@ -21,6 +24,52 @@ Page({
       that.setData({
         userInfo:userInfo
       })
+    });
+
+    that.searchForecastWeather(that.data.city);
+    that.searchTodayWeather(that.data.city);
+  },
+  searchForecastWeather(city){
+    var that = this;
+    wx.request({
+      url: 'https://free-api.heweather.com/v5/forecast?city=' + city + '&key=87b3d4c87d6c42c4b72197c0c9237ab6',
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data);
+        let daily_forecast = res.data.HeWeather5[0].daily_forecast;
+        for (let i in daily_forecast) {
+          res.data.HeWeather5[0].daily_forecast[i].date = util.formatDay(daily_forecast[i].date);
+        }
+        that.setData({
+          forecastWeather: res.data.HeWeather5[0].daily_forecast
+        })
+      },
+      error: function () {
+        console.log("weather api fail");
+      }
+    })
+  },
+  searchTodayWeather(city){
+    var that = this;
+    wx.request({
+      url: 'https://free-api.heweather.com/v5/now?city=' + city + '&key=87b3d4c87d6c42c4b72197c0c9237ab6',
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data);
+        
+        that.setData({
+          todayWeather: res.data.HeWeather5[0].now
+        })
+      },
+      error: function () {
+        console.log("weather api fail");
+      }
     })
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -16,6 +16,38 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
+function formatDay(date) {
+  let today = '';
+  switch (new Date(date).getDay()) {
+    case 0:
+      today = '星期日';
+      break;
+    case 1:
+      today = '星期一';
+      break;
+    case 2: 
+      today = '星期二';
+      break;
+    case 3:
+      today = '星期三';
+      break;
+    case 4:
+      today = '星期四';
+      break;
+    case 5:
+      today = '星期五';
+      break;
+    case 6:
+      today = '星期六';
+      break;
+    default:
+      console.log('the date is error');
+      break;
+  }
+  return today;
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatDay: formatDay
 }
